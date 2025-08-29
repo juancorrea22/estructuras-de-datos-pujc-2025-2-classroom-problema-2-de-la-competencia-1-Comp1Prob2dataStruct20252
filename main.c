b251f718271499d3fa5f03efa63004daaca0b554
juancorrea22 <167539539+juancorrea22@users.noreply.github.com>
2025-08-29 09:50:33 -0500

    Update main.c nuestro

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-    int n;
-    scanf("%d",&n);
-    // TODO: malloc n, leer arreglo
-    // TODO: imprimir en orden inverso separados por espacio
-    // TODO: free
+
+
+int main() {
+
+    long long n, i;
+    long long *l;
+
+    scanf("%lld", &n);
+
+    l = (long long *) malloc(n*sizeof(long long));
+
+    for (i = 0; i < n; i++) {
+        scanf("%lld", l + i);
+
+    }
+
+    for (i = n - 1; i > -1 ; i--) {
+        printf("%lld ", l[i]); 
+    }
     return 0;
 }
